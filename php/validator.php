f462b554e4fe81dc3c582a04f3731542e5697a77
lexus1991ru <nnov.spawn@gmail.com>
2012-11-16 22:42:48 +0400

    refactored Wrapped DB - devided by classes with using deriv...

--- php/validator.php
+++ php/validator.php
@@ -1,6 +1,38 @@
 <?php
 require_once("settings.php");
 
+class Validator
+{
+    public function validateMail($email)
+    {
+        $email = trim($email);
+        $pattern = "|^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$|";
+        if (preg_match($pattern, $email)){
+            return true;
+        }
+        return false;
+    }
 
+    public function validateUser($username)
+    {
+        // TODO: use approved symbols for checking username
+        $username = trim($username);
+        if ((strlen($username) < ServerSettings::getMinUsernameLenght()) || (strlen($username) > ServerSettings::getMaxUsernameLength()))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public function validatePass($pass)
+    {
+        $pass = trim($pass);
+        if ((strlen($pass) < ServerSettings::getMinPasswordLenght()) || (strlen($pass) > ServerSettings::getMaxPasswordLenght()))
+        {
+            return false;
+        }
+        return true;
+    }
+}
 
 ?>
